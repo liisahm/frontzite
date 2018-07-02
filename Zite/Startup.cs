eb8c03aceab9b75c57d91a164119933bd5737343
Liisa Haabpiht <liisa.haabpiht@gmail.com>
2018-07-02 13:26:04 +0300

    added https://frontzite.herokuapp.com/

--- Zite/Startup.cs
+++ Zite/Startup.cs
@@ -48,7 +48,7 @@ namespace Zite
         }
 
       app.UseCors((builder =>
-        builder.WithOrigins("http://localhost:49866")
+        builder.WithOrigins("https://frontzite.herokuapp.com/")
             .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod()));
